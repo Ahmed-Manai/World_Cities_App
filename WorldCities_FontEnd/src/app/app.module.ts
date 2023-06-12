be3b46725a12e04b6bfa1e8bd28ee9850b4bb259
Ahmed Manai <amanai@integrationobjects.com>
2023-06-12 17:06:38 +0100

    FrontEnd World Cities

---
+++ WorldCities_FontEnd/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { NavMenuComponentComponent } from './nav-menu-component/nav-menu-component.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponentComponent,
+    NavMenuComponentComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule,
+    MatButtonModule, MatIconModule, MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
