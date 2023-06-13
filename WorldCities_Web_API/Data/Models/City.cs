a60273eded8bb2d970d56271b592147e6a8e9c58
Ahmed Manai <amanai@integrationobjects.com>
2023-06-13 10:55:23 +0100

    add Data Model into Web API

---
+++ WorldCities_Web_API/Data/Models/City.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Data.Models
+{
+    [Table("Cities")]
+    [Index(nameof(Name))]
+    [Index(nameof(Lat))]
+    [Index(nameof(Lon))]
+    public class City
+    {
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lat { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column(TypeName="deciaml(7,4)")]
+        public decimal Lon { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+
+
+        #endregion
+        #region Navigation Properties
+        public Country? Country { get; set; } = null!;
+        #endregion
+    }
+}
---
+++ WorldCities_Web_API/Data/Models/Country.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Data.Models
+{
+    [Table("Countries")]
+    [Index(nameof(Name))]
+    [Index(nameof(ISO2))]
+    [Index(nameof(ISO3))]
+    public class Country
+    {
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; } = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ISO2 { get; set; } = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ISO3 { get; set; } = null;
+
+        public ICollection<City>? cities { get; set; } = null!;
+
+
+        #endregion
+    }
+}
+
